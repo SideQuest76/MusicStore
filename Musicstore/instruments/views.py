e158758e39be3f2d549c95abe45efe1ca0787b5a
SideQuest76 <krausermause.62@gmail.com>
2025-05-13 17:35:20 +0300

    laboratorna 5

--- Musicstore/instruments/views.py
+++ Musicstore/instruments/views.py
@@ -4,8 +4,12 @@ from rest_framework import status
 from django.shortcuts import get_object_or_404
 from .models import Instrument, Order
 from .serializers import InstrumentSerializer, OrderSerializer
-
-
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
+from rest_framework.views import APIView
+from rest_framework import status
+from .serializers import UserRegisterSerializer
+from .permissions import IsAdmin
 # === Instrument Views ===
 
 class InstrumentListCreateAPIView(APIView):
@@ -76,3 +80,20 @@ class OrderDetailAPIView(APIView):
         order = get_object_or_404(Order, pk=pk)
         order.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+class CustomLoginView(ObtainAuthToken):
+    def post(self, request, *args, **kwargs):
+        response = super().post(request, *args, **kwargs)
+        token = Token.objects.get(key=response.data['token'])
+        return Response({'token': token.key, 'user_id': token.user_id})
+    
+class RegisterView(APIView):
+    def post(self, request):
+        serializer = UserRegisterSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"message": "User created"}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+class SomeProtectedView(APIView):
+    permission_classes = [IsAdmin]
